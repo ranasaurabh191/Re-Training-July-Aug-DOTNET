438259e060561ecc6002c99600c818ad4f894f11
Saurabh Rana <ranasaurabh191@gmail.com>
2026-07-30 07:54:33 +0530

    Ques1

---
+++ DOTNET/FinancialCalculatorApp/FinancialCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace FinancialCalculatorApp
+{
+    static class FinancialCalculator
+    {
+        public static double CalculateCompoundInterest(double principal, double rate, int time)
+        {
+            return CalculateCompoundInterest(principal, rate, time, 1);
+        }
+
+        public static double CalculateCompoundInterest(double principal, double rate, int time, int compoundingFrequency)
+        {
+            return principal * Math.Pow(1 + (rate / compoundingFrequency), compoundingFrequency * time);
+        }
+    }
+}
---
+++ DOTNET/FinancialCalculatorApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FinancialCalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double futureValue1 = FinancialCalculator.CalculateCompoundInterest(10000, 0.05, 10);
+
+            double futureValue2 = FinancialCalculator.CalculateCompoundInterest(
+                principal: 10000,
+                rate: 0.05,
+                time: 10,
+                compoundingFrequency: 12);
+
+            Console.WriteLine($"Future Value (Annually): {System.Math.Round(futureValue1, 2)}");
+            Console.WriteLine($"Future Value (Monthly): {System.Math.Round(futureValue2, 2)}");
+        }
+    }
+}

d3b03f104111817f61c1366e133a6ba3de444459
Saurabh Rana <ranasaurabh191@gmail.com>
2026-08-01 07:40:58 +0530

    ParkingFeeCalculator completed

---
+++ DOTNET/Day_4/ParkingFeeCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Parking Fee Calculator ===");
+
+        Console.Write("Enter Vehicle Type (C/M/T): ");
+        char vehicleType = Convert.ToChar(Console.ReadLine().ToUpper());
+
+        Console.Write("Enter Parking Hours: ");
+        double hours = Convert.ToDouble(Console.ReadLine());
+
+        double rate = 0;
+        double maxFee = 0;
+
+        switch (vehicleType)
+        {
+            case 'C':
+                rate = 3;
+                maxFee = 25;
+                break;
+
+            case 'M':
+                rate = 2;
+                maxFee = 15;
+                break;
+
+            case 'T':
+                rate = 5;
+                maxFee = 40;
+                break;
+
+            default:
+                Console.WriteLine("Invalid Vehicle Type");
+                return;
+        }
+
+        double fee;
+
+        if (hours <= 0.5)
+        {
+            fee = 0;
+        }
+        else
+        {
+            fee = Math.Ceiling(hours) * rate;
+
+            if (fee > maxFee)
+                fee = maxFee;
+
+            if (hours > 8)
+                fee -= fee * 0.10;
+        }
+
+        Console.WriteLine("\n===== PARKING DETAILS =====");
+        Console.WriteLine($"Vehicle Type : {vehicleType}");
+        Console.WriteLine($"Parking Hours: {hours}");
+        Console.WriteLine($"Parking Fee  : ${fee:F2}");
+    }
+}
